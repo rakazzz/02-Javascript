87e294119e3fcfbf7901a106297679385bd8576e
rakazzz <raka.barakatuh40@gmail.com>
2021-10-05 09:35:39 -0400

    javascript

--- index.js
+++ index.js
@@ -2,23 +2,29 @@ var list = document.getElementById('list-item')
 var btnAdd = document.getElementById('btn-add-item');
 
 function inputL(){
-
-  var lit = document.createElement('li')
+  var lit = document.createElement('li');
+  lit.classList.add('pt-3', 'text-light');
   var inputItem = document.getElementById('input-item')
   var item = list.getElementsByTagName('li');
+
   var btn = document.createElement("button");
   btn.innerHTML = "delete";
-
+  btn.onclick = function () {
+    list.removeChild(lit)
+  }
+  btn.classList.add('btn', 'btn-danger')
       if (inputItem.value === '') {
         alert('tidak boleh kosong')
         inputItem.focus();
         return;
       }
+
   lit.innerHTML = inputItem.value;
   var l = String(lit.innerHTML);
   console.log(l);
   for (var i = 0; i < item.length; i++) {
-      var it = item[i].innerHTML;
+    var it = item[i].innerText;
+    it = it.substring(0, it.length - 7);
     if (l === it) {
       alert(`${l} telah ada dalam list`)
       inputItem.value = '';
@@ -27,7 +33,7 @@ function inputL(){
     }
   }
   document.getElementById('list-item').appendChild(lit);
-  document.getElementById('list-item').append(" ", btn);
+  lit.append('   ', btn)
 
   inputItem.value = '';
   inputItem.focus();
